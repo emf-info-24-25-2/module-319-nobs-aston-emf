ddf78cccef497f10b3296435a2013ad0ba95928b
Nobs-Aston-emf <Aston.nobs@studentfr.ch>
2024-09-05 10:02:21 +0200

    commit

---
+++ exercices/exercice03/TypeDeBase.java
@@ -0,0 +1,12 @@
+public class TypeDeBase{
+ public static void main(String[] args) {
+        int age = 15;
+        String nom = "Aston";
+
+       
+     System.out.printlm("Je mapelle "+ nom + " et j'ai "+ age + " ans.");
+     age += 10;
+     System.out.printlm("et dans 10ans j'aurai " + age + "ans"); 
+    }
+
+}
