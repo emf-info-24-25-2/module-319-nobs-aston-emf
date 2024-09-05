21fcaec6a6efd411699cb194c3edbac5c5081674
aston <aston.nobs@studentfr.ch>
2024-09-05 20:26:16 +0200

    maisonson

--- exercices/exercice01/Exercice02/Exercice02.java
+++ exercices/exercice01/Exercice02/Exercice02.java
@@ -2,7 +2,7 @@ package Exercice02;
 
 public class Exercice02 {
     public static void main(String[] args) {
-        System.out.println("Bonjour tout le monde");
+        System.out.println("AUREVOIR tout le monde");
     }
     
 }
