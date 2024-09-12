cf14fe80afdf2fd88553699474d851c469dab133
Nobs-Aston-emf <Aston.nobs@studentfr.ch>
2024-09-12 09:36:16 +0200

    commit

---
+++ exercices/exercice04/EchangeValeurs.java
@@ -0,0 +1,15 @@
+package exercice04;
+
+public class EchangeValeurs {
+    public static void main(String[] args) {
+       
+        int variable1 = 1;
+        int variable2 = 2;
+        System.out.println("variable 1 est "+variable1+ " variable 2 est "+variable2);
+
+
+        int Variable1 = 2;
+        int Variable2 = 1;
+        System.out.println("variable 1 est "+Variable1+ " variable 2 est " +Variable2);
+    }
+}
