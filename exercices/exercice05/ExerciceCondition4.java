7028c5d01bf79b308d92283a6a7b703f4a0169ec
Nobs-Aston-emf <Aston.nobs@studentfr.ch>
2024-09-19 10:59:52 +0200

    commit ex5

--- exercices/exercice05/ExerciceCondition4.java
+++ exercices/exercice05/ExerciceCondition4.java
@@ -1,8 +1,23 @@
 public class ExerciceCondition4 {
-public static void main(String[] args) {
-    int temerature -12;
+    public static void main(String[] args) {
 
-    if (temperature <-10) {System.out.println("il fait très froid");
-}
-}
+        int temperature = -2;
+        if (temperature < -10) {
+            System.out.println("il fait très froid");
+
+        } else if (temperature >= -10 && temperature < 0) {
+            System.out.println("il fait très froid");
+
+        } else if (temperature >= 0 && temperature < 25) {
+            System.out.println("il fait normale");
+
+        } else if (temperature >= 25 && temperature <= 35) {
+            System.out.println("il fait très froid");
+
+        } else if (temperature > 35) {
+            System.out.println("il fait très chaud");
+            // &&=et
+            // ||=ou
+        }
+    }
 }
---
+++ exercices/exercice05/condition3.java
@@ -0,0 +1,17 @@
+public class condition3 {
+
+    public static void main(String[] args) {
+
+        int valeur1 = -13;
+        int valeur2 = 3;
+
+        int multiplication = valeur1 * valeur2;
+
+        if (multiplication >= 0) {
+
+            System.out.println("Le résultat est positif");
+        } else {
+            System.out.println("Le frésultat est négatif");
+        }
+    }
+}
