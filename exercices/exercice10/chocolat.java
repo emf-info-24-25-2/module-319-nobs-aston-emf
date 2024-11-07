a153dc0a41fbb693ceb23e8e99d98a5ca26ec221
Nobs-Aston-emf <Aston.nobs@studentfr.ch>
2024-11-07 08:29:30 +0100

    07.11

---
+++ exercices/exercice10/chocolat.java
@@ -0,0 +1,9 @@
+public class chocolat {
+    public static void halloween() {
+        System.err.println("des bonbon ou un sort");
+    }
+public static void main(String[] args) {
+    halloween();
+}
+}
+
---
+++ exercices/exercice10/mathodes.java
@@ -0,0 +1,32 @@
+public class mathodes {
+    public static void main(String[] args) {
+        //methode math.min
+    System.out.println(Math.min(14, 22));
+    //sa sert a trouver le nombre le plus petit
+ 
+    //methode math.max
+    System.out.println(Math.max(14, 22));
+    //sa reprend le nombre le plus grand
+ 
+    //methode math.pow
+    System.out.println(Math.pow(2, 8));
+    // sa fait la un nombre a la puissance de l'aurtre
+ 
+    //methode math.sqrt
+    System.out.println(Math.sqrt(0));
+    //sa renvoie la racine carre du nombre
+ 
+    //methode math.abs
+    System.out.println(Math.abs(4.7));
+    //renvoie la valeur absolu d’un nombre
+    }
+
+    public static void direbonjour(String message) {
+        System.err.println(message);
+    }
+
+    public static void dire(String blabla) {
+        System.out.println("ne retourne rien");
+    }
+
+}
