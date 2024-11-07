21e2f60848ba23487ee7e4be7a64ac59a598d5b1
Nobs-Aston-emf <Aston.nobs@studentfr.ch>
2024-11-07 11:46:23 +0100

    dev 06

---
+++ devoirs/devoir06/devoir06.java
@@ -0,0 +1,18 @@
+package devoirs.devoir06;
+
+public class devoir06 {
+    int taille = 20;
+    int min = 0;
+    int max = 50;
+    int recherchee = 7;
+
+    public static void main(String[] args) {
+    }
+    
+    public static int[] generationTableu;
+    
+ 
+
+    
+}
+
