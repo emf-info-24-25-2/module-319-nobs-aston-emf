83764f420acf67636bb399b77668891690c9437b
Nobs-Aston-emf <Aston.nobs@studentfr.ch>
2024-11-08 18:03:51 +0100

    pas finit

--- devoirs/devoir06/devoir06.java
+++ devoirs/devoir06/devoir06.java
@@ -1,18 +1,35 @@
 package devoirs.devoir06;
 
 public class devoir06 {
-    int taille = 20;
-    int min = 0;
-    int max = 50;
-    int recherchee = 7;
+    public static final int TAILLE_TABLEAU = 20;
+    public static final int VALEUR_MIN = 0;
+    public static final int VALEUR_MAX = 50;
+    public static final int VALEUR_RECHERCHEE = 7;
 
     public static void main(String[] args) {
+        int[] monTableauExemple = genereTableau(3, 67, 5);
+        System.out.println(rechercheMIN(monTableauExemple));
     }
-    
-    public static int[] generationTableu;
-    
- 
 
-    
+    public static int[] genereTableau(int min, int max, int taille) {
+        int[] tab = new int[taille];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = (int) (Math.random() * (max - min + 1)) + min;
+        }
+
+        return tab;
+    }
+
+    public static int rechercheMIN(int[] tableauEntiers) {
+
+        int resultat= VALEUR_MAX;
+        for (int i = 0; i < tableauEntiers.length; i++) {
+            if (tableauEntiers[i] < resultat) {
+                resultat = tableauEntiers[i];
+            }
+        }
+
+        return resultat;
+    }
+
 }
-
